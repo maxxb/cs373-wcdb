59584664039f8431667f85e36d192f4484af1d84
alek0313 <User@wireless-10-146-177-75.public.utexas.edu>
2013-10-20 20:48:01 -0500

    gunicorn added to installed apps in settings

--- wcdb/settings.py
+++ wcdb/settings.py
@@ -129,6 +129,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crises',
+    'gunicorn'
     
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
