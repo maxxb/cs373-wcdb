d1494f08006495bb14d4ccfa0eaecf10bd679ac7
Charles Tang <ctang@cs.utexas.edu>
2013-11-13 20:37:20 -0600

    modified queries.sql

---
+++ wcdb/queries.sql
@@ -0,0 +1,18 @@
+#Select all organizations operating in multiple countries.
+SELECT org_id FROM OrganizationsData
+
+#Select everything not related to anything else.
+SELECT person_id FROM 
+#union
+SELECT org_id
+#union
+SELECT crisis_id
+
+#Select the longest running crisis.
+SELECT
+
+#Select most related (most related people/crises) organizations.
+SELECT
+
+#Count the number of crises before the 21st century (earlier than 2000).
+SELECT
