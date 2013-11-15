64170707ef738fbb1588e4e0c658c1e250dc35de
pglass <pnglass@gmail.com>
2013-11-14 18:17:33 -0600

    fixed templates

--- wcdb/crises/search.py
+++ wcdb/crises/search.py
@@ -3,17 +3,18 @@ from collections import OrderedDict
 
 def query(search_terms):
 
-	tokens = search_terms.split(' ')
-	#URL -> number of search term matches
-	URL_match_count = {}
-	for token in tokens:
-		found_URLs = index[token]
-		#for each URL that contains the curent search token
-			#add 1 to the match count
-		for URL in found_URLs:
-			if URL in URL_match_count:
-				URL_match_count[URL] += 1
-			else:
-				URL_match_count[URL] = 1
-	#returns a list of best matching URLs in order of most matches to fewest
-	return OrderedDict(sorted(URL_match_count.items(), key = lambda t: t[1])).keys()[::-1]
+    tokens = search_terms.split(' ')
+    #URL -> number of search term matches
+    URL_match_count = {}
+    for token in tokens:
+        found_URLs = index[token]
+        #for each URL that contains the curent search token
+            #add 1 to the match count
+        for URL in found_URLs:
+            if URL in URL_match_count:
+                URL_match_count[URL] += 1
+            else:
+                URL_match_count[URL] = 1
+    #returns a list of best matching URLs in order of most matches to fewest
+    print URL_match_count
+    return OrderedDict(sorted(URL_match_count.items(), key = lambda t: t[1])).keys()[::-1]
--- wcdb/crises/views.py
+++ wcdb/crises/views.py
@@ -6,30 +6,32 @@ from search import query
 ################################################
 
 def links(request,entity):
-	if entity == 'crises':
-		crises_list = Crises.objects.all()
-		return render(request,'links_page.html', {'links':crises_list})
-	elif entity == 'organizations':
-		org_list = Organizations.objects.all()
-		return render(request,'links_page.html', {'links':org_list})
-	elif entity == 'people':
-		people_list = People.objects.all()
-		return render(request,'links_page.html', {'links':people_list})
+    if entity == 'crises':
+        crises_list = Crises.objects.all()
+        return render(request,'links_page.html', {'links':crises_list})
+    elif entity == 'organizations':
+        org_list = Organizations.objects.all()
+        return render(request,'links_page.html', {'links':org_list})
+    elif entity == 'people':
+        people_list = People.objects.all()
+        return render(request,'links_page.html', {'links':people_list})
 
 def crisis_index(request, cid):
-	crisis_data = CrisesData.objects.get(pk=cid)
-	return render(request, 'crises_index.html', {'crisis_data':crisis_data})
+    crisis_data = CrisesData.objects.get(pk=cid)
+    return render(request, 'crises_index.html', {'crisis_data':crisis_data})
 
 def org_index(request, oid):
-	org_data = OrganizationsData.objects.get(pk=oid)
-	return render(request, 'org_index.html', {'org_data':org_data})
+    org_data = OrganizationsData.objects.get(pk=oid)
+    return render(request, 'org_index.html', {'org_data':org_data})
 
 def person_index(request, pid):
-	people_data = PeopleData.objects.get(pk=pid)
-	return render(request, 'people_index.html', {'people_data': people_data})
+    people_data = PeopleData.objects.get(pk=pid)
+    return render(request, 'people_index.html', {'people_data': people_data})
 
 ###########################################################################################################
 def search (request, search_terms):
-	query_results = query(search_terms)
-	return render(request, 'search.html', {'search_terms': search_terms, 'query_results': query_results})
+    print "searched: %s" % search_terms
+    query_results = query(search_terms)
+    print "results: %s" % query_results
+    return render(request, 'search.html', {'search_terms': search_terms, 'query_results': query_results})
 ############################################################################################################
